88090a9f78e5e04466e68a060070d10034480bc6
riskkim <wnsgh611@gmail.com>
2014-07-24 06:13:36 +0900

    Part 5 Complete Testing Part

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from polls.models import *
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -12,11 +13,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         #Return the last five published polls.
-        return Poll.objects.order_by('-pub_date')[:5]
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Poll
