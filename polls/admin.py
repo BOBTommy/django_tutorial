83337f4fb18f2d78c3da6374c749225f2d13d317
riskkim <wnsgh611@gmail.com>
2014-07-24 01:18:43 +0900

    Part 2 Complete Admin Page Customization

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from polls.models import Poll,Choice
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    #Inline Model -> parent - child structure
+    #Admin can modify child's model in parent modification page
+
+class PollAdmin(admin.ModelAdmin):
+
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+    fieldsets = [
+        ('Questions',           {'fields' : ['question']}),
+        ('Date information',    {'fields' : ['pub_date'], 'classes' : ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll,PollAdmin)
